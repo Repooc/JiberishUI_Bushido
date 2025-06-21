7f7b114332d59f4d533642a68163df0e573c2f78
Repooc <tjcooper1982@gmail.com>
2025-06-20 22:59:16 -0400

    cleanup

--- JiberishUI_Bushido/Init.lua
+++ JiberishUI_Bushido/Init.lua
@@ -1,10 +1,7 @@
-local E, _, V, P, G = unpack(ElvUI)
 local AddOnName, Engine = ...
 local GetAddOnMetadata = (C_AddOns and C_AddOns.GetAddOnMetadata) or GetAddOnMetadata
 
+JiberishUIBushidoDB = JiberishUIBushidoDB or {}
 Engine.Config = {}
 Engine.Config.Version = GetAddOnMetadata(AddOnName, 'Version')
-Engine.Config.TestDBInfo = GetAddOnMetadata(AddOnName, 'X-SavedVariables')
 Engine.Options = {}
-
-JiberishUIBushidoDB = JiberishUIBushidoDB or {}
